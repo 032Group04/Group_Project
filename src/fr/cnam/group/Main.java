18feb4af0c56c272d173fe50a00762a00c8fe9e5
aperikub <aperikub@hotmail.fr>
2021-12-12 10:14:17 +0100

    test ajout d'une base de donnée derby générée par intelliJ

--- src/fr/cnam/group/Main.java
+++ src/fr/cnam/group/Main.java
@@ -1,8 +1,17 @@
 package fr.cnam.group;
 
 public class Main {
-
+    SQLConnexion sqlConnexion;
     public static void main(String[] args) {
-	// write your code here
+        Main main = new Main();
+        try {
+            main.sqlConnexion = new SQLConnexion();
+        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException e) {
+            e.printStackTrace();
+        }
+
+        main.sqlConnexion.connect(SQLConnexion.host ,"admin","password");
+
+
     }
 }
---
+++ src/fr/cnam/group/SQLConnexion.java
@@ -0,0 +1,45 @@
+package fr.cnam.group;
+
+import javax.sql.DataSource;
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SQLConnexion {
+
+    public String driver = "org.apache.derby.jdbc.EmbeddedDriver";
+    public static String host = "jdbc:derby:database;create=true";
+    private Connection connection;
+    public SQLConnexion() throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        Class.forName(driver).newInstance();
+    }
+
+
+    public void connect(String url,String user, String password) {
+        try{
+            connection = DriverManager.getConnection(url, user, password);
+            if (connection.isValid(0)) {
+                JOptionPane.showMessageDialog(null,"connexion réussie","succès",
+                        JOptionPane.WARNING_MESSAGE);
+            }
+        }catch(SQLException err){
+            JOptionPane.showMessageDialog(null,"erreur de connexion","erreur",
+                    JOptionPane.WARNING_MESSAGE);
+        }
+    }
+
+    public void EndConnection(){
+        try {
+            System.out.println("end connection");
+            connection.close();
+            if(connection.isClosed()){
+                System.out.println("déconnexion réussie");
+            }
+        } catch(SQLException e){
+            System.err.println(e.getMessage());
+        }
+    }
+
+}
